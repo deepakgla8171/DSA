fc46421811ced381d3a4f2e6b8bc32f3705cf6ff
Deepak Kumar <dk1184366@gmail.com>
2026-07-25 09:48:36 +0530

    commit

---
+++ Array/SortArray.java
@@ -0,0 +1,32 @@
+package Array;
+import java.util.*;
+public class SortArray{
+   void arr1(int arr[]){
+    int temp = 0;
+      for(int i = 0; i<arr.length; i++){
+        for(int j = 1+1; j<arr.length; j++){
+             
+    if(arr[i] > arr[j]){
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp; 
+    }        
+        }
+      }
+   }
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Size ");
+    int n = sc.nextInt();
+    System.out.print("Enter Array Element ");
+    int arr[] = new int[n];
+      for(int i = 0; i<n; i++){
+        arr[i] = sc.nextInt();
+      } 
+      SortArray  obj = new SortArray();
+      obj.arr1(arr);
+      for(int i = 0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+      }
+   } 
+}
