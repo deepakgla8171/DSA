3431e3e3ce2eba199a70d21c99b934f760035dba
Deepak Kumar <dk1184366@gmail.com>
2026-07-27 19:46:32 +0530

    commit

---
+++ Array/test.java
@@ -0,0 +1,29 @@
+package Array;
+import java.util.*;
+public class test {
+    void arr1(int arr[],int sum){
+   for(int i =0; i<arr.length; i++){
+for(int j = 1; j<arr.length; j++){
+    if(arr[i] + arr[j] == sum){
+        System.out.print("sum pair "+ i);
+    }
+}
+   }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size ");
+        int n = sc.nextInt();
+        System.out.print("Enter Ele ");
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("sum ");
+        int sum = sc.nextInt();
+        test obj = new test();
+        obj.arr1(arr, sum);
+
+        
+    }
+}
