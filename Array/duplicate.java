adcad8b09c99c4b91996be18ff5f4bebb60b7e04
Deepak Kumar <dk1184366@gmail.com>
2026-07-27 19:23:15 +0530

    commit

---
+++ Array/duplicate.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class duplicate {
+    void arr1(int arr[]){
+        int du=0;
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 1; j<arr.length; j++){
+           if(arr[i] == arr[j]){
+            System.out.print(du);
+            du++;
+           }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size ");
+        int n = sc.nextInt();
+        System.out.print("Enter Array ");
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+
+        }
+         duplicate obj = new duplicate();
+         obj.arr1(arr);
+
+    }
+}
