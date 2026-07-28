253c3265d839941ab01ea5aac714d79448261497
Deepak Kumar <dk1184366@gmail.com>
2026-07-28 09:39:23 +0530

    commit

---
+++ Array/SecondLagest.java
@@ -0,0 +1,25 @@
+package Array;
+import java.util.*;
+public class SecondLagest {
+  
+    void arr1(int arr[]){
+        int max = arr[0];
+        for(int i = 0; i<arr.length; i++){
+        max = arr[arr.length-2];
+        
+        }
+         System.out.print(max);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size ");
+        int n = sc.nextInt();
+         System.out.print("Enter ele ");
+         int arr[] = new int[n];
+         for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+         }
+         SecondLagest obj = new SecondLagest();
+         obj.arr1(arr);
+    }
+}
