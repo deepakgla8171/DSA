11b832f62b8232afd348cc634f7902613a1775b5
Deepak Kumar <dk1184366@gmail.com>
2026-07-30 23:35:07 +0530

    commit

--- Array/Reversetemp.java
+++ Array/Reversetemp.java
@@ -24,7 +24,7 @@ public class Reversetemp {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+      
 
         System.out.print("Enter Size: ");
         int n = sc.nextInt();
