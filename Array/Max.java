345d429ede02249a73e37fcc116ba37311eb9d6e
Deepak Kumar <dk1184366@gmail.com>
2026-08-02 21:17:55 +0530

    commit

--- Array/Max.java
+++ Array/Max.java
@@ -5,7 +5,7 @@ public class Max {
         int max = arr[0];
         for(int i = 0; i<arr.length; i++){
             if(arr[i] > max){
-                max = arr[i];
+              
 
                
             }
