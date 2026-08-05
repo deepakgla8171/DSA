3054645d0bc7bd4649f3fe7b52832d61a8531c8b
Deepak Kumar <dk1184366@gmail.com>
2026-08-05 23:03:03 +0530

    commit

---
+++ MethodQuestion/Prime.java
@@ -0,0 +1,26 @@
+package MethodQuestion;
+public class Prime {
+
+    static boolean prime(int n) {
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        boolean ans = prime(13);
+        System.out.println(ans);
+    }
+}
