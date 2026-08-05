060cc325c605d05fece41f5d4f01cf2616d42e13
Deepak Kumar <dk1184366@gmail.com>
2026-08-05 22:20:48 +0530

    commit

---
+++ MethodQuestion/factorial.java
@@ -0,0 +1,18 @@
+package MethodQuestion;
+
+public class factorial {
+    static int fact(int n){
+        int fact = 1;
+      for(int i = 1; i<=n; i++){
+         fact = fact * i;
+        
+      }  
+       return fact;
+    
+    }
+    public static void main(String[] args) {
+         int ans = fact(5);
+         System.out.print(ans);
+
+    }
+}
