a76c0e3fe8e8b7c3cb231878f0f9a83a6f64a515
Deepak Kumar <dk1184366@gmail.com>
2026-08-19 21:56:27 +0530

    commit

--- TwoDMatrix/Rotate.java
+++ TwoDMatrix/Rotate.java
@@ -15,7 +15,13 @@ public class Rotate {
                 arr[j][i] = temp;
             }
         }
- 
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr.length/2; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][arr.length - 1 -j];
+                arr[i][arr.length - 1 - j] = temp;
+            }
+        }
         for(int i = 0; i<arr.length; i++){
             for(int j = 0; j<arr.length; j++){
                 System.out.print(arr[i][j] + " ");
