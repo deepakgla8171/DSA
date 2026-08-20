6f51f265b446fedfa7b36cf883fbaebd57e4637d
Deepak Kumar <dk1184366@gmail.com>
2026-08-21 00:06:19 +0530

    commit

--- TwoDMatrix/sum.java
+++ TwoDMatrix/sum.java
@@ -14,7 +14,7 @@ public class sum {
         System.out.println("Sum = " + sum);
     }
 
-    static void print(int arr1[][]) {
+ 
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1[i].length; j++) {
                 System.out.print(arr1[i][j] + " ");
