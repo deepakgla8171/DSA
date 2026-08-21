7690b3597f63370232270c802857283cf48eacd7
Deepak Kumar <dk1184366@gmail.com>
2026-08-21 20:48:42 +0530

    commit

--- TwoDMatrix/columwise.java
+++ TwoDMatrix/columwise.java
@@ -10,7 +10,7 @@ public class columwise {
             for(int i = 0; i<arr.length; i++){
                 System.out.print(arr[i][j] + " ");
             }
-            
+            System.out.println();
         }
         
     }
