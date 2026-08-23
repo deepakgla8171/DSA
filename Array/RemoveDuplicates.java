98785b548b03c3d066833a1a5672b57041526a07
Deepak Kumar <dk1184366@gmail.com>
2026-08-23 22:23:34 +0530

    commit

--- Array/RemoveDuplicates.java
+++ Array/RemoveDuplicates.java
@@ -12,7 +12,7 @@ public class RemoveDuplicates {
     return res;
    }
    public static void main(String[] args) {
-    int arr[] ={1,1,0,0,2,3,3,4,4};
+    
    int n = dup(arr);
     for(int i =0; i<n; i++){
         System.out.print(arr[i] + " ");
