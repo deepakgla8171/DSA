e63e5ec5bf60b0a84ea1a199d2caa45a7f3d9314
Deepak Kumar <dk1184366@gmail.com>
2026-08-24 20:54:42 +0530

    commit

--- Array/RemoveDuplicates.java
+++ Array/RemoveDuplicates.java
@@ -6,7 +6,7 @@ public class RemoveDuplicates {
     for(int i = 1; i<arr.length; i++){
         if(arr[i] != arr[res-1]){
             arr[res] = arr[i];
-          
+            res++;
         }
     }
     return res;
