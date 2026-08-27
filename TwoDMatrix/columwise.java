14235bc1dd5e50d5689401cb39aa26d0beeaf8df
Deepak Kumar <dk1184366@gmail.com>
2026-08-27 08:16:23 +0530

    commit

--- TwoDMatrix/columwise.java
+++ TwoDMatrix/columwise.java
@@ -4,7 +4,7 @@ public class columwise {
     public static void main(String[] args) {
         int arr[][] = {
             {1,2,3,4},
-      
+            {5,6,7,8}
         };
         for(int j = 0; j<arr[0].length; j++){
             for(int i = 0; i<arr.length; i++){
