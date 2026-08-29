1e3eb0f61acd808ec5222b6bd0c21a280a2035ff
Deepak Kumar <dk1184366@gmail.com>
2026-08-29 22:09:30 +0530

    commit

--- TwoDMatrix/RowWise.java
+++ TwoDMatrix/RowWise.java
@@ -8,7 +8,6 @@ public class RowWise {
     };
     for(int i = 0; i<arr.length; i++){
         for(int j = 0; j<arr[i].length; j++){
-                    System.out.print(arr[i][j] +" ");
         }
         System.out.println();
     }
