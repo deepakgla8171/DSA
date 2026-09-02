94f90f42179f850a23e68856e23bf0fea3d1f34b
Deepak Kumar <dk1184366@gmail.com>
2026-09-02 20:58:27 +0530

    commit

--- Daalab/A33_62.java
+++ Daalab/A33_62.java
@@ -8,7 +8,7 @@ public class A33_62 {
         int arr1[] = new int[n];
         int arr2[] = new int[n];
         int arr3[] = new int[n];
-         System.out.print("Enter Element Array1: ");
+        
         for(int i = 0; i < n; i++)
             arr1[i] = sc.nextInt();
        System.out.print("Enter Element Array2: ");
