86d005f39b75bee0a910b4291dc644cfcb1425a8
Deepak Kumar <dk1184366@gmail.com>
2026-09-05 21:16:10 +0530

    commit

--- Array/Max.java
+++ Array/Max.java
@@ -20,7 +20,7 @@ public class Max {
         System.out.print("Enter elemnet ");
         int arr[] = new int[n];
       
-   
+        for(int i = 0; i<n; i++){
             arr[i] = sc.nextInt();
         }
         Max obj = new Max();
