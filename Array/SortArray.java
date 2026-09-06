2c29bf0ef212e9a6010e5115cca89ce2543860b0
Deepak Kumar <dk1184366@gmail.com>
2026-09-06 21:17:01 +0530

    commit

--- Array/SortArray.java
+++ Array/SortArray.java
@@ -20,7 +20,9 @@ public class SortArray{
     int n = sc.nextInt();
     System.out.print("Enter Array Element ");
     int arr[] = new int[n];
-     
+      for(int i = 0; i<n; i++){
+        arr[i] = sc.nextInt();
+      } 
       SortArray  obj = new SortArray();
       obj.arr1(arr);
       for(int i = 0; i<arr.length; i++){
